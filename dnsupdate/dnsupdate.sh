8ba27297940897e1a2a9b17fab9c9b969326686b
azira <azira@usagi-goten.com>
2015-01-05 18:08:03 +0900

    remove &lt;

--- dnsupdate/dnsupdate.sh
+++ dnsupdate/dnsupdate.sh
@@ -3,6 +3,6 @@ LOGFILE=/home/myname/dnsupdate.log
 MYIP=$(curl -s -S http://httpbin.org/ip | grep origin | awk '{print $2}' | cut -d\" -f2)
 date +"%Y/%m%d %T" >> ${LOGFILE}
 echo ${MYIP} >> ${LOGFILE}
-curl -d MID=mydns_id &lt; -d PWD=mydns_password -d IPV4ADDR=${MYIP} -s -S http://www.mydns.jp/directip.html >> ${LOGFILE}
+curl -d MID=mydns_id -d PWD=mydns_password -d IPV4ADDR=${MYIP} -s -S http://www.mydns.jp/directip.html >> ${LOGFILE}
 unset LOGFILE
 
